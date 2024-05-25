7d18a3a319c89bd9663bfe612960f3d0202bd3bc
Udara Wickramasinghe <65730273+WickyRR@users.noreply.github.com>
2024-05-25 23:49:54 +0530

    completed

--- index.js
+++ index.js
@@ -19,11 +19,13 @@ app.use(bodyParser.urlencoded({ extended:true}));
 app.use(express.static("public"));
 
 async function getBooks(){
-    const result = await db.query("SELECT * FROM read_books");
+    const result = await db.query("SELECT * FROM read_books ORDER BY id DESC");
     console.log(result.rows);
     return result.rows;
     
-}
+};
+
+
 
 app.get("/", async(req,res)=>{
     const books = await getBooks();
@@ -37,18 +39,54 @@ app.get("/add" ,async(req,res)=>{
 });
 
 
-app.post("/add",async(req,res)=>{});
+app.post("/add",async(req,res)=>{
+    const name = req.body.name;
+    const isbn = req.body.isbn;
+    const review = req.body.review;
+    const author = req.body.author;
+    const rating = req.body.rating;
+
+    try{
+        const result = await db.query("INSERT INTO read_books(name,author_name,review,isbn,rating) VALUES($1,$2,$3,$4,$5)",[
+            name,author,review,isbn,rating
+        ]);
+        res.redirect("/");
+    }catch(err){
+        console.log(err);
+    }
+});
 
 app.post("/edit",async(req,res)=>{
     const id = req.body.book_id;
+    console.log(id);
+
     try{
         const result = await db.query("SELECT * FROM read_books WHERE id = $1;",[id]);
+        console.log(result.rows[0]);
         res.render("edit.ejs",{
             book:result.rows[0]
         });
     }catch(err){
         console.log(err);
     }
+});
+app.post("/update",async(req,res)=>{
+    const id = req.body.id;
+    const name = req.body.name;
+    const isbn = req.body.isbn;
+    const review = req.body.review;
+    const author = req.body.author;
+    const rating = req.body.rating;
+
+    try{
+        const result = await db.query("UPDATE read_books SET name = $1,author_name = $2,review = $3,isbn = $4,rating = $5 WHERE id = $6",[
+            name,author,review,isbn,rating,id
+        ]);
+        res.redirect("/");
+    }catch(err){
+        console.log(err);
+    }
+
 });
 
 app.post("/delete",async(req,res)=>{
